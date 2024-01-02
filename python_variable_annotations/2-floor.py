250beccbd2e6af10c0a5f87eb831070ffb37ee38
Nicolaslegoat <n.mancino03@gmail.com>
2024-01-02 01:42:57 -0800

    adding file

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/en python3
+"""
+module takes a float n as argument
+and returns the floor of the float.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    function floor which takes a float n as argument
+    and returns the floor of the float.
+    """
+    return (int(n))
