0157ffc39ac5f2fa46c2ce3817126e3b577200d3
Nicolaslegoat <n.mancino03@gmail.com>
2024-01-01 16:37:54 -0800

    add comm

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -2,4 +2,5 @@
 """module returns their sum as a float."""
 
 def add(a: float, b: float) -> float:
+    """function add that takes a float a and a float b as arguments and returns their sum as a float."""
     return (a + b)
