dd755c27d51ef5e7988eff69a3dc2a38736c824a
Nicolaslegoat <n.mancino03@gmail.com>
2024-01-02 16:00:03 -0800

    return

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -9,4 +9,4 @@ def sum_list(input_list: List[float]) -> float:
     """takes a list input_list of floats as argument
     and returns their sum as a float.
     """
-    return sum(imput_list)
+    return sum(input_list)
