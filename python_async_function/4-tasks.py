1b537b80b53a1ac904a24068ec0ff704aae7153c
Nicolaslegoat <n.mancino03@gmail.com>
2024-01-04 01:59:53 -0800

    adding file with func ok

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""""""
+import asyncio
+from typing import List
+
+task_wait_random = __import__("3-tasks").task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Function that should return the list of all the delays (float values).
+    The list of the delays should be in ascending order without using sort()
+    because of concurrency. SAME AS 1 TASK"""
+    task = []
+    for i in range(n):
+        task.append(task_wait_random(max_delay))
+
+    delay = await asyncio.gather(*task)
+
+    return sorted(delay)
