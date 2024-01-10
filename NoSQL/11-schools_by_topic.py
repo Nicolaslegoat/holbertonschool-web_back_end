bddde078325912a422f07d03a82922a8621c051f
Nicolaslegoat <n.mancino03@gmail.com>
2024-01-10 04:36:51 -0800

    doing with topics

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -5,5 +5,5 @@
 def schools_by_topic(mongo_collection, topic):
     """function that returns the list of school having a specific topic"""
     return mongo_collection.find(
-        {"topic": topic}
+        {"topics": topic}
     )
