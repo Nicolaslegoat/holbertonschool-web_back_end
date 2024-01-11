b36e4c0cc4c38f2862f485c01228289d869876f4
Nicolaslegoat <n.mancino03@gmail.com>
2024-01-11 14:59:09 -0800

    adding file first time test

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """"""
+        assert type(page)  is int and type(page_size) is int
+        assert page > 0 and page_size > 0
+        
+        index_page: tuple = index_range(page, page_size)
+        dataset: List = self.dataset()
+
+        current_page = dataset[index_page[0]: index_page[1]]
+        return current_page
