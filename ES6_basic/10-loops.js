15671451f831981002039e783aca969548e50e3f
Nicolaslegoat <n.mancino03@gmail.com>
2024-01-17 12:52:10 -0800

    adding func with a new_array

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default function appendToEachArrayValue(array, appendString) {
+    const new_array = []
+    for (const value of array) {
+      new_array.push(appendString + value);
+    }
+  
+    return new_array;
+  }
+  
