09acf59b511f9f43ce4eae6a6be57baa7693cad2
Nicolaslegoat <n.mancino03@gmail.com>
2024-01-17 13:55:16 -0800

    function is ok

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments: (employees) => Object.keys(employees).length,
+  }
+}
