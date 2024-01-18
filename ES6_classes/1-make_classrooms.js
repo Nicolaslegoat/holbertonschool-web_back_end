37dc140971a97411b8c01d3db47d1bbbeabbb9d7
Nicolaslegoat <n.mancino03@gmail.com>
2024-01-18 02:59:24 -0800

    syntaxe of return and func name

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,11 +1,11 @@
 /* eslint-disable */
 import ClassRoom from './0-classroom.js';
 
-export default function IntializeRooms {
+export default function initializeRooms() {
     const array= [
     new ClassRoom(19),
     new ClassRoom(20),
     new ClassRoom(34),
     ];
-    return array
+    return array;
 }
