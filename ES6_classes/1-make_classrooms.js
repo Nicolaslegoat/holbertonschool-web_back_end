ecedd6f9409ce50bdad8cacb07372b08e3562b3c
Nicolaslegoat <n.mancino03@gmail.com>
2024-01-18 02:56:19 -0800

    adding file with function should return an array of 3 ClassRoom

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import ClassRoom from '0-classroom.js';
+
+export default IntializeRooms {
+    const array= [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+    ];
+    return array
+}
