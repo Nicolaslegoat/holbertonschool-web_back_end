0352e5bbb6938b274f2335f3c54cc568cbea83e3
Nicolaslegoat <n.mancino03@gmail.com>
2024-01-18 03:49:50 -0800

    adding file with Constructor attributes: name (String) length (Number) students (array of Strings)

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,52 @@
+export default class HolbertonCourse {
+    constructor(name, length, students) {
+      this._name = HolbertonCourse.validateString(name, 'Name');
+      this._length = HolbertonCourse.validateNumber(length, 'Length');
+      this._students = HolbertonCourse.validateArray(students, 'Students');
+    }
+  
+    get name() {
+      return this._name;
+    }
+  
+    set name(new_name) {
+      this._name = HolbertonCourse.validateString(new_name, 'Name');
+    }
+  
+    get length() {
+      return this._length;
+    }
+  
+    set length(new_length) {
+      this._length = HolbertonCourse.validateNumber(new_length, 'Length');
+    }
+  
+    get students() {
+      return this._students;
+    }
+  
+    set students(new_students) {
+      this._students = HolbertonCourse.validateArray(new_students, 'Students');
+    }
+  
+    static validateString(value, propertyName) {
+      if (typeof value !== 'string') {
+        throw new TypeError(`${propertyName} Must be a string`);
+      }
+      return value;
+    }
+  
+    static validateNumber(value, propertyName) {
+      if (typeof value !== 'number') {
+        throw new TypeError(`${propertyName} Must be a number`);
+      }
+      return value;
+    }
+  
+    static validateArray(value, propertyName) {
+      if (!Array.isArray(value)) {
+        throw new TypeError(`${propertyName} Must be an array`);
+      }
+      return value;
+    }
+  }
