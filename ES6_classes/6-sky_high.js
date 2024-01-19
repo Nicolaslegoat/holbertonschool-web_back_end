d1bf4385d60c0b623afdf95ed029b521ea527d69
Nicolaslegoat <n.mancino03@gmail.com>
2024-01-19 04:38:28 -0800

    i forget the extends

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import Building from './5-building.js'
 
-export default class SkyHighBuilding {
+export default class SkyHighBuilding extends Building {
     constructor(sqft, floors) {
         super(sqft);
         this._floors = floors;
