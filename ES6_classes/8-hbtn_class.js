2b58e606f3932cd33fa9795857609ab2ada4ed74
Nicolaslegoat <n.mancino03@gmail.com>
2024-01-19 04:52:29 -0800

    adding file and func working but i have undefined for the string

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default class HolbertonClass {
+    constructor(size, location) {
+        this._size = size;
+        this._loctaion = location;
+    }
+
+    valueOf() {
+        return this._size;
+    }
+
+    toString() {
+        return this._location;
+    }
+}
