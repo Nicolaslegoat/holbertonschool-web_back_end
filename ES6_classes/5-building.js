2bfbb7dd7aa4959fa105c1f2ff9e25be897e1e04
Nicolaslegoat <n.mancino03@gmail.com>
2024-01-19 05:32:23 -0800

    syntaxe in Error

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,17 +1,17 @@
 /* eslint-disable */
 export default class Building {
     constructor(sqft) {
-        if (this.constructor !== Building && !this.evacuationWarningMessage) {
-            throw Error('Class extending Buiding must override evacuationWarnigMessage');
-        }
-        this._sqft = sqft;
+      if (this.constructor !== Building && !this.evacuationWarningMessage) {
+        throw Error('Class extending Building must override evacuationWarningMessage');
+      }
+      this._sqft = sqft;
     }
-
+  
     get sqft() {
-        return this._sqft;
+      return this._sqft;
     }
-
+  
     set sqft(new_sqft) {
-        this._sqft = new_sqft;
+      this._sqft = new_sqft;
     }
-}
+  }
