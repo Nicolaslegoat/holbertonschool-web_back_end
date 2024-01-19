4e177defa513f8a1f86417bd98d9391dc2408522
Nicolaslegoat <n.mancino03@gmail.com>
2024-01-19 05:42:06 -0800

    not correct output

--- ES6_basic/6-string-interpolation.js
+++ ES6_basic/6-string-interpolation.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 export default function getSanFranciscoDescription() {
   const year = 2017;
   const budget = {
@@ -8,6 +7,6 @@ export default function getSanFranciscoDescription() {
   };
 
   return `As of ${year}, it was the seventh-highest income county in the United States,\
-with a per capita personal income of ${budget.income}. As of 2015, San Francisco\
+with a per capita personal income of ${budget.income}. As of 2015, San Francisco \
 proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
