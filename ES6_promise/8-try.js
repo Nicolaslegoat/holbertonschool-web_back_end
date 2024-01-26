a963628bee7e5212c9986a9cd571a48cc97cfcd7
Nicolaslegoat <n.mancino03@gmail.com>
2024-01-26 08:23:40 -0800

    correct output

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default function divideFunction(numerator, denominator) {
     if (denominator === 0) {
-        throw new Error('cannot divisible by 0');
+        throw new Error('cannot divide by 0');
     } else {
         return numerator / denominator;
     }
