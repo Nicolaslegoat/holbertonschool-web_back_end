88750c2621de230d73e97b877eb22d2815ec8a3a
Nicolaslegoat <n.mancino03@gmail.com>
2024-01-30 14:25:52 -0800

    at end of line

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 export default function updateStudentGradeByCity(students, city,newGrades) {
-    return students.filter((student) => student.location === city).map((student) => {
+    return students.filter((student) => student.location === city)
+    .map((student) => {
         const found_grade = newGrades.find((grade) => grade.studentId === studentId);
         if (found_grade) {
             return { ...student, grade: found_grade.grade };
